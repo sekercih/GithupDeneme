49bbdf097f5e5872c52110bca3d70845c9419d7b
sekercih <hkkskrc@gmail.com>
2020-11-07 15:48:11 +0300

    masterdan ekeye

--- src/githupproje/Edabit01.java
+++ src/githupproje/Edabit01.java
@@ -3,6 +3,18 @@ package githupproje;
 public class Edabit01 {
 	public static void main(String[] args) {
 		System.out.println("merhaba");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		System.out.println("ekeden mastere");
+		
+
+
 	}
 
 }
