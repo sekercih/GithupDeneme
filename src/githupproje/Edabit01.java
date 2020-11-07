bd1f025c7d08cc726dc5fed9656effb9607f3c18
sekercih <67022880+sekercih@users.noreply.github.com>
2020-11-07 15:53:47 +0300

    Update Edabit01.java

--- src/githupproje/Edabit01.java
+++ src/githupproje/Edabit01.java
@@ -3,7 +3,7 @@ package githupproje;
 public class Edabit01 {
 	public static void main(String[] args) {
 		System.out.println("merhaba");
-<<<<<<< HEAD
+
 		System.out.println("ekeden mastere");
 		System.out.println("ekeden mastere");
 		System.out.println("ekeden mastere");
@@ -16,11 +16,11 @@ public class Edabit01 {
 		
 
 
-=======
+
 		
 		System.out.println("eke");
 		
->>>>>>> refs/remotes/origin/master
+
 	}
 
 }
